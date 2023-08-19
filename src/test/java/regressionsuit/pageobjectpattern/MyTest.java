2ae75e5cb50a6902348f921363ed2ca11d2e5317
attila <ablikimabdulla9393@gmail.com>
2023-08-19 20:48:00 +0300

    test file

---
+++ src/test/java/regressionsuit/pageobjectpattern/MyTest.java
@@ -0,0 +1,10 @@
+package regressionsuit.pageobjectpattern;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+    @Test
+    public void test1(){
+        System.out.println(123);
+    }
+}
